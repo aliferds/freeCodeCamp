0ae50d6235d0946c61c7ee8c28f1613845c4b8ab
Alifer Duarte Silva <aliferduartesilva@gmail.com>
2026-06-10 16:40:26 -0300

    Chore: vite configuration

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  // IMPORTANTE: mude 'nome-do-repositorio' para o nome exato do seu repo no GitHub
-  base: '/nome-do-repositorio/', 
+  base: '/freeCodeCamp/', 
 })
